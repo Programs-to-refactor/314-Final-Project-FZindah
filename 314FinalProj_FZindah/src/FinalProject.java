b493fcd5bd8ea5c716629cd0b263a6d5235601ab
azind <azind@10.0.0.19>
2021-12-11 22:05:04 -0500

    314 Final Project, Farha Zindah

---
+++ 314FinalProj_FZindah/src/FinalProject.java
@@ -0,0 +1,97 @@
+/*	Farha Zindah
+ *  12/11/2021
+ *  COSC 314 Final Project
+ *  
+ *  Given a positive integer n and a nonnegative integer not
+ *  exceeding n, find the number of r-permutations and rcombinations of a set with n elements.
+ */
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class FinalProject {
+	
+	public static void main(String[] args) {
+		
+		// test rPermutationsAndCombinations method using JUnit
+		Result result = JUnitCore.runClasses(FinalProjectTest.class);
+		
+		// print out failures if occur
+		for(Failure fail : result.getFailures()) {
+			System.out.println(fail.toString());
+		}
+		System.out.println("Program successful: " + result.wasSuccessful());
+		
+		// initialize scanner for user input
+		Scanner input = new Scanner(System.in);
+		System.out.println("---------------------------------------------\n r-permutations "
+				+ "and r-combinations calculator\n--------------------" + "-------------------------");
+		
+		// run program until user quits
+		while (true) {
+			try {
+				// get user input
+				System.out.print("Enter a positive integer (n) for num elements in set: ");
+				int n = input.nextInt();
+				System.out.print("\nEnter an integer for r: ");
+				int r = input.nextInt();
+				
+				// call function
+				BigInteger[] arr = rPermutationsAndCombinations(n, r);
+				// print out values returned
+				System.out.println("r-permutations: " + arr[0] + "\n" + "r-combinations: " + arr[1]);
+
+			} catch (Exception e) {
+				System.out.println("Please enter valid integers");
+			}
+
+			System.out.println("Try again? (yes/no)");
+			input.nextLine();
+			
+			// ask if user wants to continue
+			if (input.nextLine().equals("no")) {
+				input.close();
+				break;
+			}
+		}
+	}
+
+	/* method to find and return num r-Permutations and 
+	 * rCombinations in a set
+	 */
+	public static BigInteger[] rPermutationsAndCombinations(int n, int r) throws Exception {
+		
+		// if values not applicable, throw exception
+		if (n <= 0 || r > n || r < 0)
+			throw new Exception();
+		
+		// declare BigInteger variables for factorials
+		BigInteger nFact = BigInteger.valueOf(n);
+		BigInteger rFact = BigInteger.valueOf(r);
+		BigInteger nrFact = BigInteger.valueOf(n - r);
+		
+		// 0! = 1, so if r = 0, change to 1;
+		if (rFact.equals(new BigInteger("0")))
+			rFact = new BigInteger("1");
+
+		// loop to calculate factorials for n, r, and n-r
+		for (int i = n - 1; i > 0; i--) {
+			nFact = BigInteger.valueOf(i).multiply(nFact);
+			if (i < n - r)
+				nrFact = BigInteger.valueOf(i).multiply(nrFact);
+			if (i < r)
+				rFact = BigInteger.valueOf(i).multiply(rFact);
+		}
+
+		// implement equation for permutations
+		BigInteger rPerm = nFact.divide(nrFact);
+		// implement equation for combinations
+		BigInteger rComb = nFact.divide(rFact.multiply(nrFact));
+
+		return new BigInteger[] {rPerm, rComb};
+	}
+}
---
+++ 314FinalProj_FZindah/src/FinalProjectTest.java
@@ -0,0 +1,44 @@
+/* Farha Zindah
+ * COSC 314 Final Project
+ * 12/11/2021
+ */
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.Test;
+
+import java.math.BigInteger;
+
+// Class to test Final Project
+public class FinalProjectTest {
+	
+	// Test r-permutation values
+	@Test
+	public void testRPermutations() throws Exception {
+		// check random numbers
+		assertEquals(new BigInteger("20"), FinalProject.rPermutationsAndCombinations(5, 2)[0]);
+		// check when r = 0
+		assertEquals(new BigInteger("1"), FinalProject.rPermutationsAndCombinations(5, 0)[0]);
+		// check with big numbers
+		assertEquals(new BigInteger("331284225412682501619179520000"), 
+				FinalProject.rPermutationsAndCombinations(100, 15)[0]);
+	}
+	
+	// Test r-combination values
+	@Test
+	public void testRCombinations() throws Exception {
+		// check random numbers
+		assertEquals(new BigInteger("10"), FinalProject.rPermutationsAndCombinations(5, 2)[1]);
+		// check when r = 0
+		assertEquals(new BigInteger("1"), FinalProject.rPermutationsAndCombinations(5, 0)[1]);
+		// check with big numbers
+		assertEquals(new BigInteger("253338471349988640"), 
+				FinalProject.rPermutationsAndCombinations(100, 15)[1]);
+	}
+	
+	@Test
+	@DisplayName("doesn't accept invalid values")
+	public void Exceptions() {
+		assertThrows(Exception.class, () -> FinalProject.rPermutationsAndCombinations(0,5));
+	}
+}
